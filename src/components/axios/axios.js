c121874eaf7fa344355cbadae3c34de2a18c3f0d
Rohaan <rohaanansari321@gmail.com>
2021-07-25 02:14:15 +0530

    ..

--- src/components/axios/axios.js
+++ src/components/axios/axios.js
@@ -5,10 +5,12 @@ const token = window.localStorage.getItem('jwt');
 let env = 'prod';
 
 export default axios.create({
-  baseURL:
-    env === 'prod'
-      ? 'https://instaclone-back-end.herokuapp.com'
-      : 'http://localhost:5000',
+  //   baseURL:
+  //     env === 'prod'
+  //       ? 'https://instaclone-back-end.herokuapp.com'
+  //       : 'http://localhost:5000',
+  baseURL: 'https://instaclone-back-end.herokuapp.com',
+
   headers: {
     Authorization: token ? `Bearer ${token}` : '',
   },
